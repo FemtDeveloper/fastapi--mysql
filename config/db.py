59472be7c390d610ac83a7dff3320c4d6dacef81
Felix Miranda <feedmite@hotmail.com>
2022-03-08 18:09:24 -0500

    mysql amazon rds added

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine('mysql+pymysql://root:532526@localhost:3306/storedb')
+engine = create_engine(
+    'mysql+pymysql://admin:a532526Z@da-test.c1trjg34drld.us-east-2.rds.amazonaws.com/test')
 
 conn = engine.connect()
 
